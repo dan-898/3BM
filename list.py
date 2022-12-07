ca2c16cae1ec7894dbb795f724e95a63686c7bd3
Dan <dany.sath93@gmail.com>
2022-12-07 16:14:12 +0100

    3bm

---
+++ list.py
@@ -0,0 +1,22 @@
+#
+#
+# num = [ 0,1,2,4]
+#
+# names = ['jaja','karl','dodo']
+#
+#
+# for i in (num+names):
+#
+#
+#
+#     print(i)
+
+
+
+
+
+name = input('enter name')
+age = input ('neter age')
+
+
+print(name + 'how are you ' + age)
